d607d6314891931e8153ffbe50e9d118f182da42
Gilang Arif Prakoso <gilangarif1607@gmail.com>
2016-05-31 16:32:40 +0700

    Add files via upload

---
+++ server.php
@@ -0,0 +1,27 @@
+<?php
+require_once "lib/nusoap.php";
+function getInfo($name){
+  //provide information details
+	$data['ferry']['nama'] = 'Ferry';
+	$data['ferry']['nim'] = 'L200130083';
+	if (isset($data[$name])){
+		return json_encode($data[$name]);
+	}else{
+		return "{$name}? what kind of food is that??";
+	}
+}
+
+$server = new nusoap_server();
+$server->configureWSDL("server","urn:server");
+$server->register(
+	"getInfo",
+	array("name" => "xsd:string"),
+	array("return" => "xsd:string"),
+	"urn:one",
+	"urn:one#getInfo",
+	"rpc",
+	"encoded",
+	"get info ajalah"
+	);
+$HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
+$server->service($HTTP_RAW_POST_DATA);
